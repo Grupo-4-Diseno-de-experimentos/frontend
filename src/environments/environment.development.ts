f275321b83b27d0e08759410adeece519b18ca61
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2025-07-08 16:12:56 -0500

    feat: apiurl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //url mock
-  apiUrl: 'http://localhost:8080'
-  //apiUrl: 'https://nutrismart-dfhga8ddf6fcddbb.brazilsouth-01.azurewebsites.net/'
+  //apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://nutrismart-dfhga8ddf6fcddbb.brazilsouth-01.azurewebsites.net/'
 };
